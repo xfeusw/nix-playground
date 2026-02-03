a3137d5e5eb70ac5bc3a96f3a5acd99cdd3fd0b2
xfeusw <khamrakulovkamron@gmail.com>
2026-02-03 07:12:55 +0500

    chore: init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Nix playground";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
